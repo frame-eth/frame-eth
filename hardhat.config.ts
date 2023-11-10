d5c2c92eaced893bad922306c3583265fd460834
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2023-11-10 23:35:48 +0000

    feat: hardhat config template w/ viem

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,53 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import '@nomicfoundation/hardhat-toolbox-viem'
+import 'dotenv/config'
+import { HardhatUserConfig } from 'hardhat/config'
+
+const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-};
+  defaultNetwork: 'localhost',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+    },
+    mumbai: {
+      url:
+        process.env.POLYGON_MUMBAI_RPC_URL ||
+        'https://rpc-mumbai.maticvigil.com',
+      accounts,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+  },
+  solidity: {
+    version: '0.8.19',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    cache: './cache',
+    artifacts: './artifacts',
+  },
+  mocha: {
+    timeout: 20000,
+  },
+  sourcify: {
+    enabled: true
+  },
+}
 
-export default config;
+export default config
